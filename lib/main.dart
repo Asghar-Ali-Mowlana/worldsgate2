cad04f694242d26735ecab502fbd5ae4df9cd6d7
Asghar Ali Mowlana <alimowlana24@gmail.com>
2022-03-22 09:46:12 +0530

    Added sub-facilities category for add hotels page

--- lib/main.dart
+++ lib/main.dart
@@ -42,7 +42,7 @@ class MyApp extends StatelessWidget {
             onSurface: const Color(0xFFBA780F)),
         primarySwatch: Colors.blue,
       ),
-      home: LoginPage(),
+      home: AddHotelDetails("Oqsxr0M6r5XfEDMOqqoXZAc3mgz1"),
     );
   }
 }
