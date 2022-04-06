902747b5828be577f159c43465ab4e47b3a5a1f0
RuzniSadiq <ruznithegambit@gmail.com>
2022-04-06 13:37:16 +0400

    viewcars done yo

--- lib/widgets/header.dart
+++ lib/widgets/header.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:universal_io/io.dart' as u;
+import 'package:worldsgate/helper/responsive_helper.dart';
 
 class VendomeHeader extends StatefulWidget {
   //const VendomeHeader({Key? key}) : super(key: key);
@@ -21,45 +22,71 @@ class VendomeHeader extends StatefulWidget {
 class _VendomeHeaderState extends State<VendomeHeader> {
   @override
   Widget build(BuildContext context) {
-    return (u.Platform.operatingSystem == "android" ||
-            u.Platform.operatingSystem == "ios")
-        ? Container(
-            height: 90.0,
+    return ResponsiveWidget(
+          mobile: buildContainerHeaderContent("mobile", 60, 200, 20, 10, 13),
+          tab: buildContainerHeaderContent("tab", 120, 350, 30, 20, 14),
+          desktop: buildContainerHeaderContent("desktop",160, 470, 40, 30, 15),
+        );
+  }
+
+  Container buildContainerHeaderContent(String platform, double headerheight, double worldsgatewidth, double logoradius, double avatarradius, double fontsize) {
+    return Container(
+            height: headerheight,
             decoration: BoxDecoration(
               color: Colors.black,
-              border: Border.all(color: Color(0xFFBA780F)),
+
+              // border: Border.all(color: Color(0xFFBA780F)),
+
+              border: Border(
+                bottom: BorderSide(width: 2.0, color: Color(0xFFBA780F)),
+              ),
             ),
-            child: Column(
+            child:
+            (widget.cusaddress!="")
+                ?Column(
               children: [
-                Center(
-                  child: Image.asset(
-                    "assets/images/headerimages/worldgate.png",
-                    height: 50,
-                    width: 270,
-                  ),
-                ),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     Align(
                       alignment: Alignment.topLeft,
-                      child: Container(
-                        child: Row(
-                          children: [
-                            Image.asset(
-                              "assets/images/headerimages/location.png",
-                              width: 15,
-                            ),
-                            Text(
-                              "  ${widget.cusaddress}",
-                              style: TextStyle(color: Colors.white),
-                            )
-                          ],
+                      child: CircleAvatar(
+                        backgroundColor: Colors.black,
+                        radius: logoradius,
+                        backgroundImage: AssetImage(
+                          'assets/images/logo.jpeg',
                         ),
                       ),
                     ),
                     Align(
-                      alignment: Alignment.topRight,
+                      alignment: Alignment.topCenter,
+                      child: Image.asset(
+                        "assets/images/headerimages/worldgate.png",
+                       // height: 100,
+                        width: worldsgatewidth,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(right: 18.0),
+                      child: Align(
+                        alignment: Alignment.topRight,
+                        child: CircleAvatar(
+                          radius: avatarradius,
+                          backgroundImage: AssetImage(
+                            'assets/images/premiumbrands/premiumbrands1.jpeg',
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+
+                    Align(
+                      alignment: Alignment.topLeft,
                       child: InkWell(
                         onTap: () {
                           widget.drawer.currentState!.openEndDrawer();
@@ -70,90 +97,78 @@ class _VendomeHeaderState extends State<VendomeHeader> {
                         ),
                       ),
                     ),
+                    Padding(
+                      padding: const EdgeInsets.only(right: 18.0),
+                      child: Align(
+                          alignment: Alignment.topRight,
+                          child: Text(
+                            widget.cusname.toString(),
+                            style: TextStyle(color: Colors.white, fontSize: fontsize),
+                          )),
+                    ),
+
                   ],
                 ),
               ],
-            ),
-          )
-        : Container(
-            height: 160.0,
-            decoration: BoxDecoration(
-              color: Colors.black,
+            ):
 
-              // border: Border.all(color: Color(0xFFBA780F)),
-
-              border: Border(
-                bottom: BorderSide(width: 2.0, color: Color(0xFFBA780F)),
-              ),
-            ),
-            child: Column(
+            Column(
               children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(top: 18.0),
-                      child: Align(
-                        alignment: Alignment.topLeft,
-                        child: CircleAvatar(
-                          backgroundColor: Colors.black,
-                          radius: 40,
-                          backgroundImage: AssetImage(
-                            'assets/images/logo.jpeg',
-                          ),
-                        ),
-                      ),
-                    ),
-                    Flexible(
-                      child: Align(
-                        alignment: Alignment.topCenter,
-                        child: Image.asset(
-                          "assets/images/headerimages/worldgate.png",
-                          height: 100,
-                          width: 470,
-                        ),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 18.0, right: 18.0),
-                      child: Align(
-                        alignment: Alignment.topRight,
-                        child: CircleAvatar(
-                          radius: 30,
-                          backgroundImage: AssetImage(
-                            'assets/images/premiumbrands/premiumbrands1.jpeg',
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
                 Padding(
-                  padding: const EdgeInsets.only(right: 18.0),
+                  padding: const EdgeInsets.only(top: 8.0),
                   child: Align(
-                      alignment: Alignment.topRight,
-                      child: Text(
-                        widget.cusname.toString(),
-                        style: TextStyle(color: Colors.white),
-                      )),
-                ),
-                Align(
-                  alignment: Alignment.bottomLeft,
-                  child: Padding(
-                    padding: const EdgeInsets.only(left: 16.0, top: 5.0),
-                    child: InkWell(
-                      onTap: () {
-                        widget.drawer.currentState!.openDrawer();
-                      },
-                      child: Image.asset(
-                        "assets/images/headerimages/bar.png",
-                        width: 25,
-                      ),
+                    alignment: Alignment.topCenter,
+                    child: Image.asset(
+                      "assets/images/headerimages/worldgate.png",
+                      // height: 100,
+                      width: worldsgatewidth,
                     ),
                   ),
                 ),
+
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+
+                    Padding(
+                      padding: const EdgeInsets.only(left: 12.0),
+                      child: Align(
+                        alignment: Alignment.topLeft,
+                        child: InkWell(
+                          onTap: () {
+                            widget.drawer.currentState!.openDrawer();
+                          },
+                          child: Image.asset(
+                            "assets/images/headerimages/bar.png",
+                            width: 25,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(right: 12.0),
+                      child: Align(
+                        alignment: Alignment.topRight,
+                        child: Container(
+                          child: Row(
+                            children: [
+                              Image.asset(
+                                "assets/images/headerimages/location.png",
+                                width: 15,
+                              ),
+                              Text(
+                                "  ${widget.cusaddress}",
+                                style: TextStyle(color: Colors.white, fontSize: fontsize),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ],
-            ),
+            )
           );
   }
 }
